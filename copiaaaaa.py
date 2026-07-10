0d10c9d6adaac08e073fc0957c1dd4bcfebf51b3
a20221448-arch <a20221448@pucp.edu.pe>
2026-07-10 18:09:57 -0500

    Update copiaaaaa.py

--- copiaaaaa.py
+++ copiaaaaa.py
@@ -1,6 +1,5 @@
-import streamlit as pd
-import pandas as pd
 import streamlit as st
+import pandas as pd
 
 # Configuración de la página
 st.set_page_config(page_title="Trivia SOMP", page_icon="🩺", layout="centered")
